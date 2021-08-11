c0bd1e551b4dab9e46e4ae9ab3194fbf60bb32d8
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-08-11 14:13:02 -0300

    commit

---
+++ lampada.js
@@ -0,0 +1,8 @@
+function acender(){
+    var img = window.document.getElementById('resposta')
+    img.src = 'imagens/lampada_acesa.png'
+}
+function desligar(){
+    var img = window.document.getElementById('resposta')
+    img.src = 'imagens/lampada_apagada.png'
+}
